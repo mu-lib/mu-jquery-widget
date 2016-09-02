a68af14c63ad257d3f4f8b0cfe7b9314282bb583
Mikael Karon <mikael@karon.se>
2016-09-02 20:08:40 +0200

    feat: added mu-compose support

---
+++ compose.js
@@ -0,0 +1,23 @@
+(function(modules, root, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(modules, factory);
+  } else if (typeof module === "object" && module.exports) {
+    module.exports = factory.apply(root, modules.map(function(m) {
+      return require(m);
+    }));
+  } else {
+    root["mu-jquery-widget/compose"] = factory.apply(root, modules.map(function(m) {
+      return root[m];
+    }));
+  }
+})(["mu-compose/regexp"], this, function(regexp) {
+  return regexp(/^(on|attr|prop)\/(.+)/, function(result, data, method, type) {
+    (result.dom = result.dom || []).push({
+      "method": method,
+      "type": type,
+      "value": data.value
+    });
+
+    return false;
+  });
+});
---
+++ widget.js
@@ -0,0 +1,55 @@
+(function(modules, root, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(modules, factory);
+  } else if (typeof module === "object" && module.exports) {
+    module.exports = factory.apply(root, modules);
+  } else {
+    root["mu-jquery-widget/widget"] = factory(root.jQuery);
+  }
+}(["jquery"], this, function($) {
+  function name(ns) {
+    return this
+      .split(/\s+/)
+      .map(function(type) {
+        return type + "." + ns;
+      })
+      .join(" ")
+  }
+
+  return [
+    function($element, ns) {
+      var me = this;
+
+      me.ns = ns;
+      me.$element = $element;
+
+      $.each(me.constructor.dom || false, function(index, op) {
+        switch (op.method) {
+          case "on":
+            me.on(op.type, op.value);
+            break;
+
+          case "attr":
+            $element.attr(op.type, op.value);
+            break;
+
+          case "prop":
+            $element.prop(op.type, op.value);
+            break;
+        }
+      });
+    },
+    {
+      "on": function(events, handler) {
+        var me = this;
+
+        me.$element.on(name.call(events, me.ns), $.proxy(handler, me));
+      },
+      "off": function(events, handler) {
+        var me = this;
+
+        me.$element.off(name.call(events, me.ns), handler);
+      }
+    }
+  ]
+}));
