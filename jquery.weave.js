0e55e9d85b3d6a7c0903e409c42d8b7ef23727f9
Mikael Karon <mikael@karon.se>
2016-09-06 14:05:13 +0200

    feat: bump mu-jquery-wire to 4.0.0

--- jquery.weave.js
+++ jquery.weave.js
@@ -16,7 +16,8 @@
 ], this, function($, wire) {
   var slice = Array.prototype.slice;
   var bind = Function.prototype.bind;
-  var re = /@\d+$/;
+  var re_space = /\s+/;
+  var re_instance = /@\d+$/;
   var count = 0;
 
   function create(c, args) {
@@ -26,26 +27,31 @@
   return function(attr, callback) {
     var args = slice.call(arguments, 2);
 
-    return wire.call(this, attr, function(module, index) {
-      var self = this;
+    return wire.call(this,
+      function(name) {
+        return ($(this).attr(name) || "").split(re_space);
+      },
+      function(module, index) {
+        var self = this;
 
-      return re.test(module) ? module : $.when(callback.call(self, module, index)).then(function(result) {
-        var $element;
+        return re_instance.test(module) ? module : $.when(callback.call(self, module, index)).then(function(result) {
+          var $element;
 
-        if ($.type(result) === "function") {
-          // create instance and update `$element` and `module`
-          result = create(result, [$element = $(self), module = module + "@" + ++count].concat(args));
+          if ($.type(result) === "function") {
+            // create instance and update `$element` and `module`
+            result = create(result, [$element = $(self), module = module + "@" + ++count].concat(args));
 
-          // update attribute
-          $element.attr(attr, function(i, value) {
-            value = value.split(/\s+/);
-            value[index] = module;
-            return value.join(" ");
-          });
-        }
+            // update attribute
+            $element.attr(attr, function(i, value) {
+              value = value.split(/\s+/);
+              value[index] = module;
+              return value.join(" ");
+            });
+          }
 
-        return result;
-      });
-    });
+          return result;
+        });
+      },
+      attr);
   }
 });
