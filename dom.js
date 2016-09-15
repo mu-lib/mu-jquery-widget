32fbcec3a958e04c5bcd354341d0456f723e5b54
Mikael Karon <mikael@karon.se>
2016-09-15 12:14:39 +0200

    feat: pass object as value in blueprints
    Previus to this developers were only able to pass a function or a string
    as the value which would later be used as the value for
    $element[on|attr|prop]. With this commit we add support for passing a
    literal object that will extended and passed to the constructor as-is.
    
    With this feature bluprints can set $event.data by defining a blueprint
    like so:
    
    ```
    {
      "on/click": {
        "data": "custom data",
        "handler": function($event) {
          console.log($event.data);
        }
      }
    }
    ```

--- dom.js
+++ dom.js
@@ -1,21 +1,44 @@
-(function(modules, root, factory) {
+(function (modules, root, factory) {
   if (typeof define === "function" && define.amd) {
     define(modules, factory);
   } else if (typeof module === "object" && module.exports) {
     module.exports = factory.apply(root, modules.map(require));
   } else {
-    root["mu-jquery-widget/dom"] = factory.apply(root, modules.map(function(m) {
+    root["mu-jquery-widget/dom"] = factory.apply(root, modules.map(function (m) {
       return root[m];
     }));
   }
-})(["mu-create/regexp"], this, function(regexp) {
-  return regexp(/^(on|attr|prop)\/(.+?)(?:\((.*)\))?$/, function(result, data, method, type, args) {
-    (result.dom = result.dom || []).push({
-      "method": method,
-      "type": type,
-      "args": args,
-      "value": data.value
-    });
+})(["mu-create/regexp"], this, function (regexp) {
+  var toString = Object.prototype.toString;
+
+  function copy(o) {
+    return Object.keys(o).reduce(function (result, key) {
+      if (!result.hasOwnProperty(key)) {
+        result[key] = o[key];
+      }
+      return result;
+    }, this);
+  }
+
+  return regexp(/^(on|attr|prop)\/(.+?)(?:\((.*)\))?$/, function (result, data, method, type, args) {
+    var dom = toString.call(data.value) === "[object Object]"
+      ? data.value
+      : method === "on"
+        ? { "handler": data.value }
+        : { "value": data.value };
+
+    dom = copy.call(dom, method === "on"
+      ? {
+        "method": method,
+        "events": type,
+        "selector": args
+      }
+      : {
+        "method": method,
+        "name": type
+      });
+
+    (result.dom = result.dom || []).push(dom);
 
     return false;
   });
---
+++ tests/dom.js
@@ -0,0 +1,245 @@
+(function (modules, root, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(modules, factory);
+  } else if (typeof module === "object" && module.exports) {
+    module.exports = factory.apply(root, modules.map(require));
+  } else {
+    root["mu-jquery-widget/tests/dom"] = factory.apply(root, modules.map(function (m) {
+      return {
+        "jquery": root.jQuery,
+        "qunit": root.QUnit
+      }[m = m.replace(/^\.{2}/, "mu-jquery-widget")] || root[m];
+    }));
+  }
+})([
+  "qunit",
+  "jquery",
+  "../dom",
+], this, function (QUnit, $, dom) {
+  var root = this;
+
+  QUnit.module("mu-jquery-dom/dom");
+
+  QUnit.test("returns undefined if no match", function (assert) {
+    var o = {};
+
+    assert.expect(1);
+
+    assert.strictEqual(dom(o, {
+      "key": "value",
+    }), undefined, "return should be undefined");
+  });
+
+  QUnit.test("returns false if match", function (assert) {
+    var o = {};
+
+    assert.expect(3);
+
+    assert.strictEqual(dom(o, {
+      "key": "on/events",
+    }), false, "return should be false");
+
+    assert.strictEqual(dom(o, {
+      "key": "attr/events",
+    }), false, "return should be false");
+
+    assert.strictEqual(dom(o, {
+      "key": "prop/events",
+    }), false, "return should be false");
+  });
+
+  QUnit.module("mu-jquery-dom/dom#on");
+
+  QUnit.test("handler value", function (assert) {
+    var o = {};
+
+    assert.expect(1);
+
+    dom(o, {
+      "key": "on/events(selector)",
+      "value": "handler"
+    });
+
+    assert.deepEqual(o, {
+      "dom": [{
+        "events": "events",
+        "handler": "handler",
+        "method": "on",
+        "selector": "selector"
+      }]
+    }, "o.dom should match spec");
+  });
+
+  QUnit.test("default object value", function (assert) {
+    var o = {};
+
+    assert.expect(1);
+
+    dom(o, {
+      "key": "on/events(selector)",
+      "value": {
+        "custom": "value"
+      }
+    });
+
+    assert.deepEqual(o, {
+      "dom": [{
+        "method": "on",
+        "events": "events",
+        "selector": "selector",
+        "custom": "value"
+      }]
+    }, "o.dom should match spec");
+  });
+
+  QUnit.test("override object value", function (assert) {
+    var o = {};
+
+    assert.expect(1);
+
+    dom(o, {
+      "key": "on/events(selector)",
+      "value": {
+        "events": undefined,
+        "custom": "value"
+      }
+    });
+
+    assert.deepEqual(o, {
+      "dom": [{
+        "method": "on",
+        "events": undefined,
+        "selector": "selector",
+        "custom": "value"
+      }]
+    }, "o.dom should match spec");
+  });
+
+  QUnit.module("mu-jquery-dom/dom#attr");
+
+  QUnit.test("value", function (assert) {
+    var o = {};
+
+    assert.expect(1);
+
+    dom(o, {
+      "key": "attr/name",
+      "value": "value"
+    });
+
+    assert.deepEqual(o, {
+      "dom": [{
+        "name": "name",
+        "value": "value",
+        "method": "attr"
+      }]
+    }, "o.dom should match spec");
+  });
+
+  QUnit.test("default object value", function (assert) {
+    var o = {};
+
+    assert.expect(1);
+
+    dom(o, {
+      "key": "attr/name",
+      "value": {
+        "custom": "value"
+      }
+    });
+
+    assert.deepEqual(o, {
+      "dom": [{
+        "method": "attr",
+        "name": "name",
+        "custom": "value"
+      }]
+    }, "o.dom should match spec");
+  });
+
+  QUnit.test("override object value", function (assert) {
+    var o = {};
+
+    assert.expect(1);
+
+    dom(o, {
+      "key": "attr/name",
+      "value": {
+        "name": undefined,
+        "custom": "value"
+      }
+    });
+
+    assert.deepEqual(o, {
+      "dom": [{
+        "method": "attr",
+        "name": undefined,
+        "custom": "value"
+      }]
+    }, "o.dom should match spec");
+  });
+
+  QUnit.module("mu-jquery-dom/dom#prop");
+
+  QUnit.test("value", function (assert) {
+    var o = {};
+
+    assert.expect(1);
+
+    dom(o, {
+      "key": "prop/name",
+      "value": "value"
+    });
+
+    assert.deepEqual(o, {
+      "dom": [{
+        "name": "name",
+        "value": "value",
+        "method": "prop"
+      }]
+    }, "o.dom should match spec");
+  });
+
+  QUnit.test("default object value", function (assert) {
+    var o = {};
+
+    assert.expect(1);
+
+    dom(o, {
+      "key": "prop/name",
+      "value": {
+        "custom": "value"
+      }
+    });
+
+    assert.deepEqual(o, {
+      "dom": [{
+        "method": "prop",
+        "name": "name",
+        "custom": "value"
+      }]
+    }, "o.dom should match spec");
+  });
+
+  QUnit.test("override object value", function (assert) {
+    var o = {};
+
+    assert.expect(1);
+
+    dom(o, {
+      "key": "prop/name",
+      "value": {
+        "name": undefined,
+        "custom": "value"
+      }
+    });
+
+    assert.deepEqual(o, {
+      "dom": [{
+        "method": "prop",
+        "name": undefined,
+        "custom": "value"
+      }]
+    }, "o.dom should match spec");
+  });
+});
