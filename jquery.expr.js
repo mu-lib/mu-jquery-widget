7da0f186abdb950fba34bf4ec55c3aa6d713c840
Mikael Karon <mikael@karon.se>
2017-04-22 18:50:12 +0200

    feat: added expr
    We've added a quick way for you to locate elements that have widgets attached to them:
    
    ```javascript
    var $elements = $("<div></div><div></div>");
    var widget = new Widget($elements.eq(1), "ns");
    
    $.expr[":"].widget = expr.call($);
    
    console.log($elements.filter(":widget")); // [ $elements.eq(1) ]
    ```

---
+++ jquery.expr.js
@@ -0,0 +1,29 @@
+(function (modules, root, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(modules, factory);
+  } else if (typeof module === "object" && module.exports) {
+    module.exports = factory.apply(root, modules);
+  } else {
+    root["mu-jquery-widget/jquery.expr"] = factory.apply(root, modules);
+  }
+})([], this, function () {
+  function matches($, element, search) {
+    search = $.expando + "#" + (search || "");
+    return Object.keys($.data(element)).some(function(key) {
+      return key.startsWith(search);
+    });
+  }
+
+  return function () {
+    var $ = this;
+    return $.expr.createPseudo
+      ? $.expr.createPseudo(function(search) {
+        return function(element) {
+          return matches($, element, search);
+        }
+      })
+      : function(element, index, match) {
+          return matches($, element, match[3]);
+      };
+  }
+});
---
+++ tests/jquery.expr.js
@@ -0,0 +1,72 @@
+(function (modules, root, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(modules, factory);
+  } else if (typeof module === "object" && module.exports) {
+    module.exports = factory.apply(root, modules.map(require));
+  } else {
+    root["mu-jquery-widget/tests/jquery.expr"] = factory.apply(root, modules.map(function (m) {
+      return this[m] || root[m.replace(/^\.{2}/, "mu-jquery-widget")];
+    }, {
+        "qunit": root.QUnit,
+        "jquery": root.jQuery
+      }));
+  }
+})([
+  "qunit",
+  "jquery",
+  "../jquery.expr",
+], this, function (QUnit, $, expr) {
+  var expando = $.expando;
+  var name = "mu-jquery-widget/jquery.expr";
+
+  QUnit.module(name);
+
+  QUnit.testStart(function(details) {
+    if (details.module.startsWith(name)) {
+      $.expr[":"].widget = expr.call($);
+    }
+  });
+
+  QUnit.testDone(function(details) {
+    if (details.skipped) {
+      return;
+    }
+    else if (details.module.startsWith(name)) {
+      delete $.expr[":"].widget;
+    }
+  });
+
+  QUnit.test("not matching selector", function (assert) {
+    assert.expect(1);
+
+    var $element = $("<div>");
+
+    assert.notOk($element.is(":widget"), "selector does not match");
+  });
+
+  QUnit.test("matching selector", function (assert) {
+    assert.expect(1);
+
+    var $element = $("<div>").data(expando + "#test", "test");
+
+    assert.ok($element.is(":widget"), "selector matches");
+  });
+
+  QUnit.test("matching search selector", function (assert) {
+    assert.expect(1);
+
+    var $element = $("<div>").data(expando + "#test", "test");
+
+    assert.ok($element.is(":widget(tes)"), "selector matches");
+  });
+
+  QUnit.test("matching selector for several $elements", function (assert) {
+    assert.expect(1);
+
+    var $elements = $("<div></div><div></div><div></div>");
+    var $element1 = $elements.eq(0).data(expando + "#test1", "test1");
+    var $element2 = $elements.eq(2).data(expando + "#test2", "test2");
+
+    assert.deepEqual($elements.filter(":widget").get(), [ $element1.get(0), $element2.get(0) ] , "finds matching elements");
+  });
+});
